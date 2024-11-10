68640ba58b20cf60653ae0772556d5e551c3fce2
Sarvaiya Hiren <sarvaiyahiren816@gmail.com>
2024-11-10 15:11:06 +0530

    Node-Js

---
+++ project/Pr.1 Create Server/index.js
@@ -0,0 +1,38 @@
+let http = require('http');
+
+let port = 9000;
+let fs = require('fs');
+
+let requesthandler = (req,res) =>{
+    let filename = "";
+    switch(req.url){
+        case '/':
+            filename = "./Home.html"
+        break;
+        case '/about':
+            filename = "./about.html"
+        break;
+        case '/gallery':
+            filename = "./gallery.html"
+        break;
+        case '/contact':
+            filename = "./contact.html"
+        break;
+        case '/login':
+            filename = "./login.html"
+        break;
+    }
+    fs.readFile(filename,(err,data)=>{
+        if(err){
+            console.log(err);
+            return false;
+        }
+        res.end(data);
+    })
+}
+
+let server = http.createServer(requesthandler);
+
+server.listen(port,(err)=>{
+    console.log(`server is ruunning : ${port}`);
+});
