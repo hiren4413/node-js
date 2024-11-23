16743c32ddb63472c1c57fa568144203e73007d6
Sarvaiya Hiren <sarvaiyahiren816@gmail.com>
2024-11-23 17:33:20 +0530

    todo list

--- project/Pr.2 To Do List/index.js
+++ project/Pr.2 To Do List/index.js
@@ -1,35 +1,81 @@
-    const express = require('express');
+const express = require("express");
 
-const app = express();
+let code = 8000;
 
-const port = 8000;
 
-app.set('view engine', 'ejs')
+let app = express();
 
-let toDo = [];
+app.set("view engine" , "ejs");
 
-app.get('/', (req, res)=>{
-    return res.render("view", {toDo})
-})
+let toDo = [] ;
 
-// add task
-app.post('/addList', (req, res)=>{
-    const {work} = req.body;
+app.use(express.urlencoded());
+
+app.get("/", (req,res)=>{
+    return res.render("view",{
+        all:toDo
+    });
+});
+
+// =============== Add ToDo ===============
+
+app.post("/addTask", (req,res)=>{
+    const {work, end} = req.body;
     let task ={
         id: Math.floor(Math.random()*100000),
         work: work,
+        end:end
     }
-
     toDo.push(task);
 
     console.log("Task Added successfully");
+
+    return res.redirect("/"); 
+});
+
+//  =============== Delete ToDo ===============
+
+app.get("/deleteTask", (req,res)=>{
+    let id = req.query.delId;
+    let deletedTodo = toDo.filter(val => val.id != id)
+    toDo = deletedTodo;
+    console.log("Task Deleted Successfully.");
     return res.redirect("/");
 })
 
-app.listen(port, (err)=> {
-    if(err){
-        console.log(err);
+//  =============== Edit ToDo ===============
+
+app.get('/editTask',(req,res)=>{
+    let single = toDo.find(val => val.id == req.query.editId);
+    return res.render('edit',{
+        single
+    })
+});
+
+//  =============== Update ToDo ===============
+
+app.post('/updateTask',(req,res)=>{
+    let {editId, work, end} = req.body;
+    let update = toDo.map((val)=>{
+        if(val.id == editId){
+            val.work = work,
+            val.end=end
+        }
+        return val;
+    })
+    toDo = update;
+    console.log("Your Task has now updated.");
+    
+    return res.redirect('/');
+});
+
+// listen code
+
+app.listen(code,(err) => {
+    if(err) {
+        console.log(err); 
         return false;
     }
-    console.log(`server start at port :- ${port}`);
-})
+    console.log(` Server start on port :- ${code} `);
+    
+});
